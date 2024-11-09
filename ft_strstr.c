5b2a188758552c0dd697228243c4f3b8d1400086
Beaumanoir <tbeauman@Ordinateur-portable-de-Beaumanoir.local>
2024-11-09 09:35:47 +0100

    a

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: tbeauman <tbeauman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 14:16:38 by tbeauman          #+#    #+#             */
-/*   Updated: 2024/11/07 14:24:50 by tbeauman         ###   ########.fr       */
+/*   Updated: 2024/11/09 09:22:16 by tbeauman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 char	*ft_strstr(const char *s1, const char *s2)
 {
-	if (s2 == NULL || ft_strcmp(s2, "") == 0)
+	if (ft_strcmp(s2, "") == 0)
 		return ((char*)s1);
 	if (ft_strlen(s1) < ft_strlen(s2))
 		return (NULL);
 	while (ft_strncmp(s1, s2, ft_strlen(s2)) != 0 && *s1)
 		s1++;
-	return (*s1 ? (char*)s1 : NULL);
+	if (*s1)
+		return (char *)s1;
+	return (NULL);
 }
 
 char	*ft_strnstr(const char *s1, const char *s2, size_t n)
@@ -38,5 +40,7 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 	}
 	if (i + ft_strlen(s2) - 1 == n)
 		return (NULL);
-	return (*s1 ? (char*)s1 : NULL);
+	if (*s1)
+		return (char *)s1;
+	return (NULL);
 }
